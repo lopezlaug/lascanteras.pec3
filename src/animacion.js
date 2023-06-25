ed94f07a54f82150ab12e2967a4553c3f8e7c935
Laura <lopez.laug@gmail.com>
2023-06-25 21:44:34 +0200

    info2

--- src/animacion.js
+++ src/animacion.js
@@ -1,11 +1,2 @@
 //console.log("hola mundo")
 import "./style.css";
-
-window.setInterval(() => {
-  const animacion = document.querySelector("#animacioncss");
-  if (animacion) {
-    animacion.classList.toggle("fin");
-  }
-}, 2500);
-
-//console.log(Swiper)
